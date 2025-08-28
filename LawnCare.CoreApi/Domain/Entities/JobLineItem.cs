e3a1f7ccdf06aef5fb7b18830ed5d94fe7aead76
joe <joelowrance@gmail.com>
2025-08-27 20:27:07 -0400

    Begin stubbing out estimate process

--- LawnCare.CoreApi/Domain/Entities/JobLineItem.cs
+++ LawnCare.CoreApi/Domain/Entities/JobLineItem.cs
@@ -6,42 +6,7 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace LawnCare.CoreApi.Domain.Entities
 {
-	public class JobNote : Entity, IAuditable
-	{
-		public DateTimeOffset CreatedAt { get; set; }
-		public DateTimeOffset UpdatedAt { get; set; }
-		public string Note { get; private set; } = string.Empty;
-		public JobId JobId { get; internal set; } = null!;
 
-		// EF core
-		private JobNote()
-		{
-		}
-
-		public JobNote(JobId jobId, string note)
-		{
-			JobId = jobId ?? throw new ArgumentNullException(nameof(jobId));
-			Note = note ?? throw new ArgumentNullException(nameof(note));;
-		}
-	}
-	
-	public class JobNoteConfiguration : IEntityTypeConfiguration<JobNote>
-	{
-		public void Configure(EntityTypeBuilder<JobNote> builder)
-		{
-			builder.ToTable("JobNotes", "public");
-			builder.HasKey(x => x.Id);
-			builder.Property(x => x.Note)
-				.IsRequired()
-				.HasMaxLength(-1)
-				.HasComment("The note");
-			
-			// Indexes
-			builder.HasIndex(js => js.JobId)
-				.HasDatabaseName("IX_JobNotes_JobId");
-		}
-	}
-	
 	
 	
 	public class JobLineItem : Entity, IAuditable
---
+++ LawnCare.CoreApi/UseCases/SubmitEstimate.cs
@@ -0,0 +1,62 @@
+﻿using LawnCare.Shared.Endpoints;
+using LawnCare.Shared.MessageContracts;
+
+using MediatR;
+
+namespace LawnCare.CoreApi.UseCases;
+
+public class SubmitEstimate : IEndpoint
+{
+
+	/// <param name="app"></param>
+	public void MapEndpoint(IEndpointRouteBuilder app)
+	{
+		app.MapPost("/estimate", async (IMediator mediator, JobEstimate estimate) =>
+		{
+			await mediator.Send(new SubmitEstimateCommand(estimate));
+			// needs a better result than just a status code
+			return Results.Created();
+		});
+	}
+}
+
+
+public record SubmitEstimateCommand(JobEstimate Estimate) : IRequest;
+
+public record SubmitEstimateCommandHandler : IRequestHandler<SubmitEstimateCommand>
+{
+	public Task Handle(SubmitEstimateCommand request, CancellationToken cancellationToken)
+	{
+		throw new NotImplementedException();
+	}
+}
+
+/// <summary>
+/// An "estimate" from the field is generally in agreement with the customer.  The
+/// rep has provided a price for the services requested by the client, and the two
+/// are in agreement on the cost.  A best-guess job schedule day may also be provided,
+/// but this can be highly dependent on weather, and staff/equipment availability 
+/// </summary>
+public class JobEstimate
+{
+	public string UserId { get; set; } = string.Empty; 
+	//public string TenantId { get; set; } = null!;
+	public string CustomerFirstName { get; set; }= string.Empty;
+	public string CustomerLastName { get; set; }= string.Empty;
+	public string CustomerAddress1 { get; set; }= string.Empty;
+	public string CustomerAddress2 { get; set; }= string.Empty;
+	public string CustomerAddress3 { get; set; }= string.Empty;
+	public string CustomerCity { get; set; }= string.Empty;
+	public string CustomerState { get; set; }= string.Empty;
+	public string CustomerZip { get; set; }= string.Empty;
+	public string CustomerHomePhone { get; set; }= string.Empty;
+	public string CustomerCellPhone { get; set; }= string.Empty;
+	public string CustomerEmail { get; set; }= string.Empty;
+	public DateTimeOffset ScheduledDate { get; set; }
+	public decimal EstimatedCost { get; set; }
+	public int EstimatedDuration { get; set; }
+	public string Description { get; set; }= string.Empty;
+	public List<JobServiceItem> Services { get; set; } = [];
+}
+
+public record JobEstimateLineItem(string ServiceName, int Quantity, string Comment, decimal Price); 
